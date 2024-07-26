49b5018041a18f91f6557061aa2a8957bbcd05fe
pkovdrysh <kovdryshforwork.com>
2024-07-26 03:58:58 +0300

    User, UpdateListener

--- src/main/java/org/memes/db/User.java
+++ src/main/java/org/memes/db/User.java
@@ -5,17 +5,27 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class User {
-    private boolean singedIN;
+    private boolean isSingedIn;
     private boolean nothingRequired;
     private boolean ifNicknameRequired;
     private boolean ifPhotoRequired;
+    private boolean ifListenerRequired;
+    private boolean ifRmListenerRequired;
+    private boolean ifSteamOpened;
     private boolean started;
     private Long chatID;
     private String nick;
     private String password;
 
-    public boolean isSingedIN() {
-        return singedIN;
+    public User(Long chatID, String nick, Boolean started) {
+        this.chatID = chatID;
+        this.nick = nick;
+        this.started = started;
+    }
+
+
+    public boolean isIfListenerRequired() {
+        return ifListenerRequired;
     }
 
     public boolean isNothingRequired() {
@@ -29,12 +39,23 @@ public class User {
     public boolean isIfPhotoRequired() {
         return ifPhotoRequired;
     }
+    public boolean isIfRmListenerRequired() {
+        return ifRmListenerRequired;
+    }
+
+    public void setIfSteamOpened(boolean ifSteamOpened) {
+        this.ifSteamOpened = ifSteamOpened;
+    }
+
+    public boolean isIfSteamOpened() {
+        return ifSteamOpened;
+    }
 
     public Boolean isStarted() {
         return started;
     }
 
-    private boolean isSingedIn;
+
     private ArrayList<Long> chatsId;
 
     public Long getChatID() {
@@ -57,7 +78,7 @@ public class User {
         isSingedIn = singedIn;
     }
     public User(User user){
-        this(user.getChatID(), user.getNick(), user.getPassword(), user.isStarted());
+        this(user.getChatID(), user.getNick(), user.isStarted());
     }
 
     public User(long chatID, String nick) {
@@ -68,16 +89,69 @@ public class User {
 
     public User(Long chatID) {
         User newUser = UserDao.getDao().getUserById(chatID);
+        this.chatID = chatID;
         if(newUser != null) {
-            this.chatID = chatID;
             this.nick = newUser.getNick();
             this.password = newUser.getPassword();
             this.started = newUser.isStarted();
             this.isSingedIn = true;
         }
+        else{
+            this.isSingedIn = false;
+            this.started = true;
+        }
+    }
+
+    public void ifNothingRequired() {
+        if(!ifNicknameRequired && !ifPhotoRequired && !ifListenerRequired && !ifRmListenerRequired) {
+            this.nothingRequired = true;
+        }
+        else{
+            this.nothingRequired = false;
+        }
+    }
+    public void setIfRmListenerRequired(boolean ifRmListenerRequired) {
+        if(ifRmListenerRequired) {
+            this.nothingRequired = false;
+        }
+        this.ifRmListenerRequired = ifRmListenerRequired;
+    }
+    public void setIfListenerRequired(boolean ifListenerRequired) {
+        if(ifListenerRequired) {
+            this.nothingRequired = false;
+        }
+        this.ifListenerRequired = ifListenerRequired;
+    }
+
+    public void setIfNicknameRequired(boolean ifNicknameRequired) {
+        if(ifNicknameRequired) {
+            this.nothingRequired = false;
+        }
+        this.ifNicknameRequired = ifNicknameRequired;
+    }
+
+    public void setIfPhotoRequired(boolean ifPhotoRequired) {
+        if(ifPhotoRequired) {
+            this.nothingRequired = false;
+        }
+        this.ifPhotoRequired = ifPhotoRequired;
+    }
+
+    public void setStarted(boolean started) {
+        this.started = started;
     }
 
     public ArrayList<Long> getChatsId() {
+        if( chatsId == null ) {chatsId = new ArrayList<>();}
         return chatsId;
+    }
+    public void newSession(){
+        this.setIfNicknameRequired(false);
+        this.setIfPhotoRequired(false);
+        this.setIfListenerRequired(false);
+        this.setIfRmListenerRequired(false);
+        this.setIfSteamOpened(false);
+        this.setStarted(true);
+        this.ifNothingRequired();
     }
 }
--- src/main/java/org/memes/update/UpdateListener.java
+++ src/main/java/org/memes/update/UpdateListener.java
@@ -11,6 +11,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,11 +25,8 @@ public class UpdateListener implements LongPollingSingleThreadUpdateConsumer {
             String message = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
             if(message.equals("/start")){
-                if(!started){
-                    nothingRequired = true;
-                    ifNicknameRequired = false;
-                    ifPhotoRequired = false;
-                    started = true;
+                if(!user.isStarted()){
+                    user.newSession();
                 }
                 else{
                     SendMessage alreadyStartedMessage = SendMessage
@@ -46,7 +44,155 @@ public class UpdateListener implements LongPollingSingleThreadUpdateConsumer {
                 }
             }
             if(message.equals("/setNickname")){
+                String setNicknameMessage;
+                if(user.isNothingRequired() && user.getNick() == null){
+                    setNicknameMessage = "Choose your nickname!";
+                    user.setIfNicknameRequired(true);
+                }
+                else{
+                    if(user.getNick() != null){
+                        setNicknameMessage = "Nickname already taken!";
+                    }
+                    else{
+                        setNicknameMessage = "Finish previous action!";
+                    }
+                }
+                SendMessage sendErrMessage = SendMessage
+                        .builder()
+                        .chatId(user.getChatID())
+                        .text(setNicknameMessage)
+                        .build();
+                try{
+                    client.execute(sendErrMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println("Send failed!");
+                }
+            }
+            if(message.equals("/addListener")){
+                String addListenerMessage;
+                if(user.isNothingRequired() && user.isSingedIn()){
+                    addListenerMessage = "Enter nickname of your new listener!";
+                    user.setIfListenerRequired(true);
+                }
+                else{
+                    if(!user.isSingedIn()){
+                        addListenerMessage = "Please set a nickname!";
+                    }
+                    else{
+                        addListenerMessage = "Finish previous action!";
+                    }
+                }
+                SendMessage sendMessage = SendMessage
+                        .builder()
+                        .chatId(user.getChatID())
+                        .text(addListenerMessage)
+                        .build();
+                try{
+                    client.execute(sendMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println("Send failed!");
+                }
+            }
+            if(message.equals("/removeListener")){
+                String removeListenerMessage;
+                if(user.isNothingRequired() && user.isSingedIn()){
+                    ArrayList<Long> arr = user.getChatsId();
+                    if(arr.size() > 0){
+                        removeListenerMessage = "Your listeners list: ";
+                        for(Long l : arr){
+                            removeListenerMessage += new User(l).getNick() + "  ";
+                        }
+                        user.setIfRmListenerRequired(true);
+                    }
+                    else{
+                        removeListenerMessage = "Your listeners list is empty";
+                    }
+                }
+                else{
+                    if(!user.isSingedIn()){
+                        removeListenerMessage = "Please set a nickname!";
+                    }
+                    else{
+                        removeListenerMessage = "Finish previous action!";
+                    }
+                }
+                SendMessage sendMessage = SendMessage
+                        .builder()
+                        .chatId(user.getChatID())
+                        .text(removeListenerMessage)
+                        .build();
+                try{
+                    client.execute(sendMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println("Send failed!");
+                }
+            }
+            if(message.equals("/startStream")){
+                String startStreamMessage;
+                if(user.isNothingRequired() && user.isSingedIn() && user.isStarted() && !user.isIfSteamOpened()){
+                    user.setIfSteamOpened(true);
+                    startStreamMessage = "Streaming started!";
+                }
+                else{
+                    if(!user.isSingedIn()){
+                        startStreamMessage = "Please set a nickname!";
+                    } else if (!user.isNothingRequired()) {
+                        startStreamMessage = "Finish previous action!";
+                    } else if (user.isIfSteamOpened()) {
+                        startStreamMessage = "Steam`s already opened!";
 
+                    } else{
+                        startStreamMessage = "Start your session before starting streaming!";
+                    }
+                }
+                SendMessage sendMessage = SendMessage
+                        .builder()
+                        .chatId(chatId)
+                        .text(startStreamMessage)
+                        .build();
+                try{
+                    client.execute(sendMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println("Send failed!");
+                }
+            }
+            if(message.equals("/stopStream")){
+                String stopStreamMessage;
+                if(user.isNothingRequired() && user.isSingedIn() && user.isStarted() && user.isIfSteamOpened()){
+                    user.setIfSteamOpened(false);
+                    stopStreamMessage = "Streaming stopped!";
+                }
+                else{
+                    if(!user.isSingedIn()){
+                        stopStreamMessage = "Please set a nickname!";
+                    } else if (!user.isNothingRequired()) {
+                        stopStreamMessage = "Finish previous action!";
+                    } else if (!user.isIfSteamOpened()) {
+                        stopStreamMessage = "Steam`s already stop!";
+
+                    } else{
+                        stopStreamMessage = "Start your session before starting streaming!";
+                    }
+                }
+                SendMessage sendMessage = SendMessage
+                        .builder()
+                        .chatId(chatId)
+                        .text(stopStreamMessage)
+                        .build();
+                try{
+                    client.execute(sendMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println("Send failed!");
+                }
+            }
+            if(message.equals("/stop")){
+                user.newSession();
+                user.setStarted(false);
             }
         }
 
